4550355046949340e15232710a59d56096839ccc
Martin Kravec <kravec.martin@gmail.com>
2015-06-25 09:32:41 +0200

    Autocomplete cs translation added

--- js/jquery.tokeninput.js
+++ js/jquery.tokeninput.js
@@ -21,9 +21,9 @@ var DEFAULT_SETTINGS = {
     jsonContainer: null,
 
 	// Display settings
-    hintText: "Type in a search term",
-    noResultsText: "No results",
-    searchingText: "Searching...",
+    hintText: "Zadejte jméno nebo číslo",
+    noResultsText: "Žádné výsledky",
+    searchingText: "Hledám...",
     deleteText: "&times;",
     animateDropdown: true,
 
